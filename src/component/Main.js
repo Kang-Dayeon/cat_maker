3e8ee9adb868a423f976c2d946489a1729b04c4b
강다연 <dksldiss89@gmail.com>
2023-01-18 17:43:30 +0900

    이미지추가,데이터 셋팅

--- src/component/Main.js
+++ src/component/Main.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Main = () => {
   return (
     <>
-      <h1>main</h1>
+      <h1>Cat Maker</h1>
     </>
   )
 }
--- src/data/cat_List.js
+++ src/data/cat_List.js
@@ -2,37 +2,31 @@ export const catList = [
   {
     id: 1,
     name: "치즈",
+    gender: "여",
+    image: '../image/cat01.jpg',
     age: 1,
     weight: 1,
-    state: [
-      {
-        pat: false,
-        die: false
-      }
-    ]
+    pat: false,
+    die: false
   },
   {
     id: 2,
     name: "쿠로",
+    gender: "남",
+    image: '../image/cat02.jpg',
     age: 1,
     weight: 1,
-    state: [
-      {
-        pat: false,
-        die: false
-      }
-    ]
+    pat: false,
+    die: false
   },
   {
     id: 3,
-    name: "시로",
+    name: "콩떡",
+    gender: "여",
+    image: '../image/cat03.jpg',
     age: 1,
     weight: 1,
-    state: [
-      {
-        pat: false,
-        die: false
-      }
-    ]
+    pat: false,
+    die: false
   }
 ]
